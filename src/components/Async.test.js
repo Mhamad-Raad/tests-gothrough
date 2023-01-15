0f61b6b56ab1b803fe0f82ade5cc37d8ba46ee06
joker69216 <hamaraad883@gmail.com>
2023-01-15 14:07:37 +0300

    replace fetching test with mock data

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,12 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import Async from './Async';
 
+describe('Async Component', () => {
+  // it('should render the asynchrnous code', async () => {
+  //   render(<Async />)
 
-describe("Async Component", () => {
-  it('should render the asynchrnous code', async () => {
-    render(<Async />)
+  //   const listItemElements = await screen.findAllByRole('listitem');
+  //   expect(listItemElements).not.toHaveLength(0);
+  // })
 
+  it('should render asynchronous code (MOCKED)', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ name: 'Mhamad' }],
+    });
+
+    render(<Async />);
     const listItemElements = await screen.findAllByRole('listitem');
     expect(listItemElements).not.toHaveLength(0);
-  })
-})
+  });
+});
