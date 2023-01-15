c5f14e5592ab150cfe862765428b4c6ac912d925
joker69216 <hamaraad883@gmail.com>
2023-01-15 13:41:21 +0300

    add asynchronous test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Greeting from './components/Greeting';
+import Async from './components/Async';
 import './App.css';
 
 function App() {
-  return <Greeting />;
+  return (
+    <>
+      <Greeting />
+      <Async />;
+    </>
+  );
 }
 
 export default App;
--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,2 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Async from './Async';
+
+
+describe("Async Component", () => {
+  it('should render the asynchrnous code', async () => {
+    render(<Async />)
+
+    const listItemElements = await screen.findAllByRole('listitem');
+    expect(listItemElements).not.toHaveLength(0);
+  })
+})
